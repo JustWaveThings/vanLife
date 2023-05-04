c64a5956cd2b9283b2ab7f878e924e5b4610fdec
JustWaveThings <justwavethings@proton.me>
2023-05-04 10:29:01 -0400

    Up to start of vanlife site

---
+++ scrimba_react_router_v6/src/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+import './meyer.css';
+
+function About() {
+	return <div className="test">About Page</div>;
+}
+
+export default About;
--- scrimba_react_router_v6/src/App.jsx
+++ scrimba_react_router_v6/src/App.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import './meyer.css';
 import './index.css';
+import Home from './Home';
+import About from './About';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
-	return <div className="test">TEST TEST TEST</div>;
+	return (
+		<BrowserRouter>
+			<nav>
+				<Link
+					to="/scrimba_react_router_v6/"
+					className="link"
+				>
+					{' '}
+					Home
+				</Link>
+				<Link
+					to="scrimba_react_router_v6/about"
+					className="link"
+				>
+					About
+				</Link>
+			</nav>
+			<Routes>
+				<Route
+					path="/scrimba_react_router_v6/"
+					element={<Home />}
+				/>
+				<Route
+					path="scrimba_react_router_v6/about"
+					element={<About />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
