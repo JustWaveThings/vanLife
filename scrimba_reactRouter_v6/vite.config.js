e89e27edda76bf01300ddc739b39830c2f8e61f3
JustWaveThings <justwavethings@proton.me>
2023-05-20 11:15:07 -0400

    Update project files to match previous project deployed to GH pages

--- scrimba_reactRouter_v6/vite.config.js
+++ scrimba_reactRouter_v6/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/scrimba_react_router_v6/',
+	base: '/scrimba_reactRouter_v6/',
 	plugins: [react()],
 });
