bb6830508a3e5daee67da01c4b07fa60492b3a19
JustWaveThings <justwavethings@proton.me>
2023-07-07 09:46:41 -0400

    Update project name

--- scrimba_reactRouter_v6/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-	base: '/scrimba_reactRouter_v6/',
-	plugins: [react()],
-});
