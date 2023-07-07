c5ef7be4a314748dd51d5a1400c0c5bd86653108
JustWaveThings <justwavethings@proton.me>
2023-07-07 09:57:41 -0400

    Update CTA link on home

--- vanLife/src/pages/Home.jsx
+++ vanLife/src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
-	return (
-		<div className="test home">
-			<div className="cta">
-				<h1 className="title">You got the travel plans, we got the travel vans.</h1>
-				<p>
-					Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road
-					trip.
-				</p>
-			</div>
-			<button>Find your van</button>
-		</div>
-	);
+  return (
+    <div className='test home'>
+      <div className='cta'>
+        <h1 className='title'>You got the travel plans, we got the travel vans.</h1>
+        <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+      </div>
+      <Link to='vans' className='cta-button'>
+        Find your van
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
