70e855e26cea7a54673890bae2fe5659fcefe2dd
JustWaveThings <justwavethings@proton.me>
2023-11-12 11:21:37 -0500

    update hero image link to work on deploy

--- vanLife/src/pages/About.jsx
+++ vanLife/src/pages/About.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 
 function About() {
-	return (
-		<div className="about">
-			<div className="hero-img">
-				<img
-					src="./src/images/person_on_top_of_van.png"
-					alt="person on top of camper van at night"
-				/>
-			</div>
-			<main>
-				<header>Don’t squeeze in a sedan when you could relax in a van.</header>
-				<p>
-					Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before
-					each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)
-				</p>
-				<p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
-			</main>
-			<div className="cta-cont-about">
-				<div className="cta-text-about">Your destination is waiting. Your van is ready.</div>
-				<button className="cta-text-button">Explore our vans</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className='about'>
+      <div className='hero-img'>
+        <img
+          src='~/../src/images/person_on_top_of_van.png'
+          alt='person on top of camper van at night'
+        />
+      </div>
+      <main>
+        <header>Don’t squeeze in a sedan when you could relax in a van.</header>
+        <p>
+          Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs
+          extra 😉)
+        </p>
+        <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+      </main>
+      <div className='cta-cont-about'>
+        <div className='cta-text-about'>Your destination is waiting. Your van is ready.</div>
+        <button className='cta-text-button'>Explore our vans</button>
+      </div>
+    </div>
+  );
 }
 
 export default About;
